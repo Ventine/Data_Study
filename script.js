fc03fe32d62af3e0141e870e1855b40722cc01d3
Ventine <jhondragon15@gmail.com>
2023-10-25 20:44:47 -0500

    Portafolio vs 1.0

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("php");
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("htmlcss");
+        habilidades[3].classList.add("angular");
+        habilidades[4].classList.add("spring");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[7].classList.add("pensamiento");
+        habilidades[8].classList.add("aprendizaje");
+        habilidades[9].classList.add("resolucion");
     }
 }
 
